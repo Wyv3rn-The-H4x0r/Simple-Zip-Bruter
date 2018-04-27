ac2de9c2a9f63aeac200346194f4482125d9e7a5
Wyv3rn-The-H4x0r <Dragony12345@gmail.com>
2018-04-27 04:39:45 +0200

    Add files via upload

---
+++ Bruter.py
@@ -0,0 +1,36 @@
+import zipfile
+import itertools
+import string
+from threading import Thread
+
+print(""" 
+ **************
+ * Zip-Bruter *
+ *    Tak     *
+ *  V 1.0.0   *
+ **************
+	""")
+
+list = raw_input("Input Full Dictionary filename *pw_list.txt* :\n")
+file = raw_input("Bruting Target *test.zip* :\n")
+
+def dictionary():
+    print("Open Password-List..\n ")
+    passwords = open(list)
+    print("[+] Start Bruting.... ")
+    for line in passwords.readlines():
+        pwd = line.strip('\n')
+        print(pwd)
+        t = Thread(target=crack, args=(zipFile, pwd))
+        t.start()
+
+def crack(zip, pwd):
+    try:
+        zip.extractall(pwd=str.encode(pwd))
+        print('\n[+] Success: Password is ' + pwd)
+        t.stop()
+    except:
+        pass
+
+zipFile = zipfile.ZipFile(file)
+dictionary()
